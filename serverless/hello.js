a09edeacaf433c0e1c26106e10b0cde98aa8888b
alissaVrk <alissa.vrk@gmail.com>
2020-06-25 16:50:50 +0300

    aaa

--- serverless/hello.js
+++ serverless/hello.js
@@ -1,8 +1,10 @@
 // this uses the callback syntax, however, we encourage you to try the async/await syntax shown in async-dadjoke.js
-module.exports = function handler(event, context, callback) {
+module.exports = {
+  handler: function handler(event, context, callback) {
     console.log('queryStringParameters', event.queryStringParameters)
     callback(null, {
       statusCode: 200,
       body: JSON.stringify({ msg: 'Hello, World!' }),
     })
-  }
+  } 
+} 
